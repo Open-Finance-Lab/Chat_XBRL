4204469dff1ef24306df703f03005cd7d73fd94e
williamryanfernando <156137397+williamryanfernando@users.noreply.github.com>
2024-10-11 17:23:10 -0400

    Updated Colab code
    updated colab code to work with multiple CIK and create subdirectories to download the files to.

--- CIK/Colab_Code.py
+++ CIK/Colab_Code.py
@@ -187,7 +187,11 @@ def download_sec_files(link, download_dir):
 # folder, xbrl_file = download_sec_files(urls[1], download_dir)
 # print(f"Folder: {folder}, XBRL File: {xbrl_file}")
 
-''' cik_number; 
+# List of tickers and years
+
+# Loop through each ticker and year to download XBRL files
+
+''' cik_number;
 print(f"CIK Number for {cik_number}")
 cik_number = '0001326801'
 urls = load_10k_xbrl(cik_number)
@@ -195,18 +199,37 @@ print(f"URLs for {company_name}: {urls}") '''
 
 input_file_path = '/3_test.txt'
 
+''' print(f"CIK Number for {cik_number}")
+cik_number = '0001326801'
+urls = load_10k_xbrl(cik_number)
+print(f"URLs for {company_name}: {urls}")
+
+#download_dir = '.'  # Current working directory
+download_dir = '/content'
+for url in (urls):
+  folder, xbrl_file = download_sec_files(url, download_dir)
+  print(f"Folder: {folder}, XBRL File: {xbrl_file}") '''
+
 try:
     with open(input_file_path, 'r') as file:
         # Loop through each line in the file
         for line in file:
             cik_number = line.strip()  # Remove any leading/trailing whitespace
-
+            print("CIK NUMBER CHANGED TO {}".format(cik_number))
             print(f"CIK Number for {cik_number}")
-            cik_number = '0001326801'
             urls = load_10k_xbrl(cik_number)
             print(f"URLs for {company_name}: {urls}")
 
-            download_dir = '.'  # Current working directory
+            sub_dir = cik_number
+            parent_dir = "/content"
+
+            full_path = os.path.join(parent_dir, sub_dir)
+
+            # Create the subdirectory
+            os.mkdir(full_path)
+
+            #download_dir = '.'  # Current working directory
+            download_dir = full_path
             for url in (urls):
               folder, xbrl_file = download_sec_files(url, download_dir)
               print(f"Folder: {folder}, XBRL File: {xbrl_file}")
@@ -221,6 +244,5 @@ except Exception as e:
 for url in (urls):
   folder, xbrl_file = download_sec_files(url, download_dir)
   print(f"Folder: {folder}, XBRL File: {xbrl_file}") '''
-
 
 
