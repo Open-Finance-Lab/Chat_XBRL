73bd584118efed8a5a721e9d3951cb4703c8a812
williamryanfernando <156137397+williamryanfernando@users.noreply.github.com>
2024-12-08 22:50:58 -0500

    Updated the Parser
    The parser was working for only one specific input file and I updated it to take in an input text file and updated the output file based on those. Also added documentation to the parser to explain why the output file is arranged the way it is and what the 10K xbrl extraction file does with it.

--- cik_parser.py
+++ cik_parser.py
@@ -1,6 +1,10 @@
 import requests
 import json
 
+#The goal of this parser is to take the input file produce an output json file to be used by the webscraper
+#The result looks like the following:         "CIK": "0001993586", "company_name": "KERSHAW DAVID"
+#The reason this was picked was because the 10K XBRL extraction file takes cik and name in order to make specific files. 
+
 def extract_cik_from_txt(file_path):
     ciks = []
     with open(file_path, 'r') as file:
@@ -8,9 +12,11 @@ def extract_cik_from_txt(file_path):
             # Split the line by the colon and get the last part
             parts = line.strip().split(':')
             if len(parts) > 1:
+                company_name = parts[0].strip()
                 cik = parts[1].strip()  # Get the CIK part and remove whitespace
                 if cik.isdigit():  # Check if the CIK is numeric
-                    ciks.append(cik)
+                    ciks.append({"CIK" : cik,
+                                 "company_name" : company_name})
     return ciks
 
 def write_ciks_to_json(cik_numbers, output_file):
@@ -23,6 +29,9 @@ def main(txt_file, output_file):
     print(f"Extracted CIK numbers written to {output_file}")
 
 if __name__ == "__main__":
-    txt_file_path = './cik-lookup-data-3.txt'  # Update with your file path
-    output_file_path = './cik_numbers.json'  # Specify the output JSON file path
+    #generalize the input json file. the input file needs to be in the same folder as this code. 
+
+    txt_file_path = input("Enter the JSON FILE: ").strip()
+    txt_file_path += '.txt'  # Update with your file path
+    output_file_path = './cik_numbers.json'  # Output JSON file 
     main(txt_file_path, output_file_path)
